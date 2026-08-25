e6230d8b256f678f0531d2d98518e6c365cc2167
Shark-empire <babor3331@icloud.com>
2026-08-25 20:10:42 +0000

    Refactor AST structure and add new node types

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,14 +1,37 @@
 #[derive(Debug, Clone)]
 pub enum Node {
-    Pipeline(Vec<Command>),
-    // Future: List(Box<Node>, ListOp, Box<Node>), Subshell(Box<Node>)
+    Pipeline(Pipeline),
+    /// left && right  |  left || right
+    AndOr(Box<Node>, ListOp, Box<Node>),
+    /// left ; right
+    Sequence(Box<Node>, Box<Node>),
+    /// node &
+    Background(Box<Node>),
+    /// ( node )
+    Subshell(Box<Node>),
+    /// { node; }
+    BraceGroup(Box<Node>),
+    If(IfClause),
+    While(WhileClause),
+    For(ForClause),
+    Function(FunctionDef),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ListOp { And, Or }
+
+#[derive(Debug, Clone)]
+pub struct Pipeline {
+    pub commands: Vec<SimpleCommand>,
+    pub negated: bool, // leading `!`
 }
 
 #[derive(Debug, Clone)]
-pub struct Command {
+pub struct SimpleCommand {
+    /// VAR=value prefixes
+    pub assignments: Vec<(String, String)>,
     pub args: Vec<String>,
     pub redirects: Vec<Redirect>,
-    pub background: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -16,4 +39,30 @@ pub enum Redirect {
     Input(String),
     Output(String),
     Append(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct IfClause {
+    pub condition: Box<Node>,
+    pub then_branch: Box<Node>,
+    pub else_branch: Option<Box<Node>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct WhileClause {
+    pub condition: Box<Node>,
+    pub body: Box<Node>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ForClause {
+    pub var: String,
+    pub words: Vec<String>,
+    pub body: Box<Node>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FunctionDef {
+    pub name: String,
+    pub body: Box<Node>,
 }
