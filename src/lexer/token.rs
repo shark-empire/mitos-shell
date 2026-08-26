d6b790fbae17b7025462af3383f0911aa60a8f89
Shark-empire <babor3331@icloud.com>
2026-08-26 17:41:55 +0000

    Update token.rs

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,6 +1,8 @@
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Word(String),
+    SingleQuoted(String), // Prevents variable/glob expansion
+    DoubleQuoted(String), // Allows variable expansion, prevents globbing
     Pipe,           // |
     And,            // &&
     Or,             // ||
